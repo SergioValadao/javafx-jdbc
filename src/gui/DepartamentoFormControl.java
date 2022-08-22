0f0a199194fbb97a3a7174a0b2add7771a44a832
SergioValadao <sergiovaladao@gmail.com>
2022-08-22 12:19:25 -0300

    Validation exception

--- src/gui/DepartamentoFormControl.java
+++ src/gui/DepartamentoFormControl.java
@@ -3,19 +3,24 @@ package gui;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.ResourceBundle;
+import java.util.Set;
 
 import connection.DbException;
 import gui.listenner.DataChangeListenner;
+import gui.util.Alertas;
 import gui.util.TxtRestricao;
 import gui.util.Utils;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import model.entities.Departamento;
+import model.exceptions.ValidationException;
 import model.services.DepartamentoService;
 
 public class DepartamentoFormControl implements Initializable {
@@ -58,10 +63,16 @@ public class DepartamentoFormControl implements Initializable {
 	
 	@FXML
 	private void onbtSalvarAction(ActionEvent event) {
+		try {
 		this.departamento = getFormData();		
 		this.service.SalvarDepartamento(departamento);
 		notifyDataChangeListenner();
 		Utils.currentStage(event).close();
+		}catch(ValidationException e) {
+			setErrorMessages(e.getErrors());		
+		}catch(DbException e) {
+			Alertas.showAlert("Erro", "Departamento", e.getMessage(), AlertType.ERROR);
+		}
 	}
 	
 	private void notifyDataChangeListenner() {
@@ -92,9 +103,22 @@ public class DepartamentoFormControl implements Initializable {
 	
 	public Departamento getFormData() {
 		Departamento dep = new Departamento();
+		ValidationException exception = new ValidationException("Nome de departamento invalido!");
+		if(txtDepartamento.getText()==null || txtDepartamento.getText().trim() == "") {
+			exception.addErrors("Departamento", exception.getMessage());
+		}
 		dep.setId(Utils.ParseInteger(txtId.getText()));
 		dep.setName(txtDepartamento.getText());
+		if(exception.getErrors().size()>0) {
+			throw exception;
+		}		
 		return dep;		
+	}
+	private void setErrorMessages(Map<String, String> errors) {
+		Set<String> fields = errors.keySet();
+		if(fields.contains("Departamento")) {
+			lblErro.setText(errors.get("Departamento"));
+		}
 	}
 
 }
